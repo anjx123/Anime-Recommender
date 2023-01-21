e0e1ab2e32443e1409135fbf31de8c5d2316c39b
Ruby Liu <rl.liuruby@gmail.com>
2023-01-21 11:41:41 -0800

    Arbitrary fixing of long comment & personal test

--- src/App.js
+++ src/App.js
@@ -6,27 +6,15 @@ function App() {
     <div className="App">
       {
       /* 
-      PEPEGA 
-      BUT
-      COOLER
-      AND
-      MORE
-      VERTICAL
-      YEP
-      And
-      comes
-      with 50%
-      more 
-      air.
+      yep, just making a comment change is all. 
+      maybe we should delete the pepega branch later
       */
-
-
       }
 
       <div className="test-change">
         <img 
           className="test" src="https://media.tenor.com/kIieuwK2ZR8AAAAd/among-us-whip-nae-nae.gif"
-          alt="text"
+          alt="insert alt text here"
         />
       </div>
 
