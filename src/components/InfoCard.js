2a774da9c70fda310c99f0811c7612d2f0dd09d2
Anrui Liu <anjx101@gmail.com>
2023-01-21 23:10:11 -0800

    Link is nicer and looks cool

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -7,7 +7,13 @@ import Paper from '@mui/material/Paper';
 import { maxHeight } from '@mui/system';
 
 function InfoCard(props) {
-    const title = props.anime.node.title;
+    const id = props.anime.node.id
+    const onClickHandler = () => {
+        window.open("https://myanimelist.net/anime/".concat(id))
+
+    }
+    var title = props.anime.node.title === null ? "": props.anime.node.title;
+    title = title.length > 20 ? `${title.substring(0, 20)}...`: title;
     const imageUrl = props.anime.node.main_picture.large;
     var synopsis = props.anime.node.synopsis === null ? "": props.anime.node.synopsis;
     synopsis = synopsis.length > 50 ? `${synopsis.substring(0, 50)}...`: synopsis;
@@ -15,19 +21,24 @@ function InfoCard(props) {
     return (
         <div className="InfoCard-container">
             <div className="InfoCard-set">
-                <Paper>
+                <Paper className="animeCard__paper">
                 <img 
                     src={imageUrl} 
                     alt={title}
                     style={{ maxHeight: 300}}
                     height="400"
                 />
-                </Paper>
+                
 
                 <Typography variant="h5" component="h2"> {title} </Typography>
                 <Typography variant="body2" component="h2" paragraph={true}>
                     {synopsis}
                 </Typography>
+                <Link component="button" variant="body1" style={{marginBottom: 0}} onClick={onClickHandler}>
+                    Visit Website
+                </Link>
+
+                </Paper> 
 
         
             </div>
