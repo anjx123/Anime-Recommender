c7e710228f7631ae21fa9eda216a5e9da3667673
anjx123 <114792511+anjx123@users.noreply.github.com>
2023-01-26 23:07:04 -0800

    Update AnimeList.js

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -45,33 +45,7 @@ const AnimeList = (props) => {
         setCurrentRec(smth)     
     }
     
-    // if (!test) {
-    //     console.log(test)
-    //     return <div>
-    //          <button onClick={handleSearch} variant="h1">
-    //         <Typography>
-    //             Recommendations
-    //         </Typography>
-    //        </button>
-    //         Could not Found
-    //     </div>
-    // }
 
-
-
-
-    // artificially added in from master branch 10:32am
-    // {los = props.data.map((anime) => (
-    //     anime.list_status.score
-    // ))}
-    // const d = props.data;
-    // const g = grouper(los);
-    // const p = percentage(los, g);
-    // const r = relative(p);
-    // const ag = animGrouper(d);
-
-    // console.log(r + "aaa")
-    // console.log(ag + "bbb")
     
     
 
@@ -87,15 +61,7 @@ const AnimeList = (props) => {
             </Typography>
            </button>
 
-        // THE 3 LINES OF CODE BELOW ALLOW FOR BASIC FUNCTIONALITY OF CREATING A LIST OF ANIME ON A PERSON'S PROFILE
-        <ImageList>
-            {props.data.map((anime) => (<InfoCard key={anime.node.id} anime={anime}/>))}
-        </ImageList>
-
-        // // // FOLLOWING CODE BLOCK IS ARTICIALLY MERGED. 10:33am 
-        //  <ImageList variant="standard">     
-            
-        //      <Tagger ag={ag} r={r}/>
+  
 
             {currentRec.map((anime) => {
                 console.log("in Currect rec");
@@ -104,40 +70,9 @@ const AnimeList = (props) => {
             })}
 
 
-        //  </ImageList>
+        </ImageList>
 
-        
-        // // EVERYTHING ELSE COMMENTED OUT BELOW IS OLD CODE FROM EARLIER ITERATIONS
-
-        // // <ImageList variant="standard">     
-        // //     <Grouper data={props}/>
-           
-        // //     {los = props.data.map((anime) => (
-        // //         anime.list_status.score
-        // //     ))}
-        
-        // <ImageList variant="standard">     
-            
-        //     <Tagger ag={ag} r={r}/>
-          
-
-        // //     {/* {log = props.data.map((anime) => (
-        // //         anime.node.genres[0].name
-        // //     ))} */}
-
-
-        // //     {relative(percentage(los, grouper(los)))}
-
-
-
-        // //     {/* {props.data.map((anime) => (
-        // //         anime.list_status.score
-        // //     ))},
-        // //     {props.data.map((anime) => (
-        // //         anime.node.genres[0].name
-        // //     ))} */}
-
-        // //  </ImageList>
+       
     );
 };
 
